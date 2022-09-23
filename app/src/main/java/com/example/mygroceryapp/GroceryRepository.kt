f0b8c3b6d6a6e42210b3b04a796500fd8d6d4c9d
surajyadav1108 <harrypotter1108@gmail.com>
2022-09-24 01:13:38 +0530

    My-Grocry-App

---
+++ app/src/main/java/com/example/mygroceryapp/GroceryRepository.kt
@@ -0,0 +1,9 @@
+package com.example.mygroceryapp
+
+class GroceryRepository(private val db:GroceryDataBase) {
+
+    suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
+    suspend fun delete(items: GroceryItems) = db.getGroceryDao().delete(items)
+
+    fun getAllItems() = db.getGroceryDao().getAllGroceryItems()
+}
---
+++ app/src/main/java/com/example/mygroceryapp/GroceryViewModalFactory.kt
@@ -0,0 +1,12 @@
+package com.example.mygroceryapp
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class GroceryViewModalFactory(private  val repository: GroceryRepository) : ViewModelProvider.NewInstanceFactory(){
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        return GroceryViewModal(repository)as T
+    }
+}
